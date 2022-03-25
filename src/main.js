60c0d39115c7d17e96fb6757c2652173e7a7336a
#80H@ <innit@duck.com>
2022-03-25 18:50:16 +0000

    edited stuff to make it better

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
 const logger = require('./lib/logger/logger');
 let locale = "main";
-const os-util = require("os-utils")
+const os = require("os-utils")
 const getSplash = require('./lib/splasher/splasher')
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Menu, ipcMain } = require('electron');
 const url = require('url')
 const path = require('path');
 
 
 
-if (require('electron-squirrel-startup')) { 
 
+if (require('electron-squirrel-startup')) { 
   app.quit();
 }
 
@@ -18,32 +18,34 @@ const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
-    webPreferences: {nodeIntegration: true,devTools: false,}
+    webPreferences: {nodeIntegration: true},
+    autoHideMenuBar: true
   });
-  mainWindow.loadFile(path.join(__dirname, './view/basewin/basewin.html'));
-//  mainWindow.webContents.openDevTools();
+  mainWindow.loadFile(path.join(__dirname, './view/main/index.html'));
+  mainWindow.webContents.openDevTools();
 };
 
-app.on('ready', () => {
-  logger('ready', locale);
-  createWindow()
-});
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
 
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    logger('window-all-closed', locale)
     app.quit();
   }
 });
 
 app.on('activate', () => {
-  logger("activate", locale);
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
 });
 
-app.on('quit', () => {
-  logger("app-quit", locale)
-});
-
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and import them here.
---
+++ src/view/main/script.js
@@ -0,0 +1,5 @@
+const getSplash = require("../../lib/splasher/splasher");
+
+function splash(){
+    alert(getSplash())
+}
